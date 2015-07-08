b3f07c823a4cd63c30bdb88010230c8a9c92fe02
phunghangbk <phunghangbk@gmail.com>
2015-07-08 16:06:43 -0400

    Show user profile

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
     resources :categories, except: :show
     resources :users, only: [:index, :update, :destroy]
   end
-
+  
+  resources :users, only: :show
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
