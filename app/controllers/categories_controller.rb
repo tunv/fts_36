3ccb0802ab581c2a8f531fef8c3de9ce3ec9203f
phunghangbk <phunghangbk@gmail.com>
2015-07-15 10:43:01 -0400

    search category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,12 @@ class CategoriesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @categories = @categories.paginate page: params[:page]
+    @search = Category.search params[:q]
+    @categories = @search.result.page params[:page]
+
+    respond_to do |format|
+      format.html
+      format.json {render json: @categories}
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find params[:id]
+    @search = @user.exams.joins(:category).search params[:q]
+    @exams = @search.result.page params[:page]
   end
 end
